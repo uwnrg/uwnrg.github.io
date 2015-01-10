e11f4b3593753538580903094bede7722f833d44
Sahibjot Saggu <sahibjot@live.ca>
2015-01-09 19:53:34 -0500

    mobile testing

--- new/script.js
+++ new/script.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 
 	// check if the user is on mobile
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- 		$("#img_banner").css("height", "200px");
+ 		//$("#img_banner").css("height", "200px");
+ 		alert("hey");
 	}	
 
 
