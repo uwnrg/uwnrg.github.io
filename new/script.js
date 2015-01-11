5de31aee8e83b98f34e48759157ff24bfb35a8a0
Sahibjot Saggu <sahibjot@live.ca>
2015-01-11 00:26:41 -0500

    working on the mobile version of the main page

--- new/script.js
+++ new/script.js
@@ -10,30 +10,10 @@ $(document).ready(function() {
 		}
 	});
 
-	//$("#logo a").html($("main").height() + ", " +$("#img_banner").height() + ", " + $("header").height());	
-
-
 	$("footer").css("top", $("#img_banner").height() + $("main").height() - 96 + "px");
-
-/*
-	$("#post_title").html($("#img_banner").css("height"));
-	$("footer").css("top", $("main").height() + "px");*/
 
 	$(window).resize(function() {
 		$("footer").css("top", $("header").height() + $("#img_banner").height() + $("main").height() + "px");
-		$("#logo a").html($("main").height() + ", " +$("#img_banner").height() + ", " + $("header").height());	
+		//$("#logo a").html($("main").height() + ", " +$("#img_banner").height() + ", " + $("header").height());	
 	})
-
-/*
-	$(window).resize(function() {
-		//$("#banner_wrapper span").html($(window).width());
-		$("#post_title").html($(window).width());
-		$("footer").css("top", 70 + $("#img_banner").height() + $("main").height() + "px");
-		if ($(window).width() > 475 && $(window).width() < 840) {
-			$("main").css("background-color", "red");
-		}
-		else {
-			$("main").css("background-color", "#CCC");
-		}	
-	});*/
 });
