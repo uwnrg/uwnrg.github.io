e651b865576bfab2e3948ef36a7be7e5222db32f
aneedalie <t236liu@uwaterloo.ca>
2016-03-16 19:57:21 -0700

    Delete old files

--- script.js
+++ script.js
@@ -1,66 +1,74 @@
 $(document).ready(function() {
-
-	$("#nav_button").click(function() {
-		if ($("#topnav").css("display") == "none") {
-			$("#topnav").css("display", "block");
-		}
-		else {
-			$("#topnav").css("display", "none");
-		}
-		/*if ($("#topnav").css("display", "block")) {
-			$("#topnav").css("display", "none");
-		}*/
-		/*if ($("#topnav").css("top") != "70px") {
-			$("#topnav").css('top', '70px');
-		}
-		else {
-			$("#topnav").css('top', '-250px');
-		}*/
-
+	$("#loading_bar").animate({
+		'width' : '100%'
+	}, 4000);
+	$("#loading_screen").delay(4000).fadeOut(1000);
+	$("#f_o_more_button").click(function() {
+		window.location = "posts.html";
 	});
-/*
-	// Loading Animation
-	$("#loading_bar").animate({'width':'100%'}, 1500, "easeInExpo");
-	$("#loading_bar").animate({'height':'100%', 'top':'0%'});
-	$("#loading_bar").fadeOut();
-	$("aside").fadeIn();
-	$("aside").delay(1500).animate({'left':'0%'});
-	$("main").delay(2000).animate({'right':'0%'});
-
-*/
-
-
-/*
-	if ($(document).width() <= 1525) {
-		$("aside #title").animate({'left':'20px'});
-		$("aside #options ul li").animate({'text-indent':'50px'});
+	function close_all() {
+		$("#michelle_info").fadeOut();
+		$("#sam_info").fadeOut();
+		$("#saw_info").fadeOut();
+		$("#maya_info").fadeOut();
+		$("#dark_page").fadeOut();
 	}
-
-	if ($(document).width() <= 1250) {
-		$("aside").fadeOut();
-		$("main").animate({'width':'100%'});
-		/* bring down navbar 
-	}
-
-*/
-
-	$(window).resize(function() {
-		$("#wrapper1 span").html($(window).width());
-		$("#team_banner span").html($(window).width());
-/*
-		// PAGE RESIZE HERE ........ ALSO GOTTA DO THIS ON PAGE LOAD
-
-		if ($(document).width() <= 1525) {
-			$("aside #title").animate({'left':'20px'});
-			$("aside #options ul li").animate({'text-indent':'50px'});
+	$(".robot_info_close").click(function() {
+		close_all();
+	});
+	$("#dark_page").click(function() { close_all(); });
+	$("#michelle").click(function() {
+		$("#dark_page").fadeIn();
+		$("#michelle_info").fadeIn();
+	});
+	$("#sam").click(function() {
+		$("#dark_page").fadeIn();
+		$("#sam_info").fadeIn();
+	});
+	$("#saw").click(function() {
+		$("#dark_page").fadeIn();
+		$("#saw_info").fadeIn();
+	});
+	$("#maya").click(function() {
+	$("#dark_page").fadeIn();
+	$("#maya_info").fadeIn();
+	});
+	$(document).scroll(function() {
+		if ($(document).scrollTop() - 50 >= 0) {
+			$("#front").css("position", "fixed");
+			$("#front").css("top", "0px");
+			$("#front").css("background", "#222");
 		}
+		else {
+			$("#front").css("position", "absolute");
+			$("#front").css("top", "50px");
+			$("#front").css("background", "transparent");
+		}
+	});
 
-		if ($(document).width() <= 1250) {
-			$("aside").fadeOut();
-			$("main").animate({'width':'100%'});
-			/* bring down navbar 
-		}*/
-
+	
+	var post_list = document.getElementById('all_posts').getElementsByTagName("li");
+	$(".post_title").click(function(event) {
+		var post_id = jQuery(this).attr("id");
+		$("#" + post_id + "_view").fadeIn();
+		$("#" + post_id + "_view #" + post_id + "_view_title").html($("#" + post_id).text());
+		$("#" + post_id + "_view #" + post_id + "_view_date").html($("#" + post_id + "_date").text());
+	});
+	$(".post_view #close_button").click(function() {
+		for (var each_post = 1; each_post < post_list.length; each_post++) {
+			if ($("#post" + each_post + "_view").css("display") == "block") {
+				$("#post" + each_post + "_view").fadeOut();	
+			}
+		}
+	});
+	$(document).keyup(function(e) {
+		if (e.keyCode == 27) {
+			for (var each_post = 1; each_post < post_list.length; each_post++) {
+				if ($("#post" + each_post + "_view").css("display") == "block") {
+					$("#post" + each_post + "_view").fadeOut();	
+				}
+			}
+		}
 	});
 
 });
